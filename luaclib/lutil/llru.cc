f75db77cbf2a8d92f2befb809894c0bae8da6fda
Monsoon <97245589@.qq.com>
2025-09-05 13:21:29 -0600

    rank lru

--- luaclib/lutil/llru.cc
+++ luaclib/lutil/llru.cc
@@ -3,43 +3,68 @@ extern "C" {
 #include "lua.h"
 }
 
-#include "lru.hpp"
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 static const char *LLRU_META = "LLRU_META";
+
+struct Lru {
+  list<string> ids_;
+  unordered_map<string, list<string>::iterator> list_it_;
+  int num;
+
+  void update(const string &id, string &evict) {
+    if (auto it = list_it_.find(id); it != list_it_.end()) {
+      ids_.erase(it->second);
+      list_it_.erase(it);
+    }
+    ids_.push_front(id);
+    list_it_[id] = ids_.begin();
+
+    if (list_it_.size() > num) {
+      auto it = ids_.rbegin();
+      evict = *it;
+      list_it_.erase(*it);
+      ids_.pop_back();
+    }
+  }
+
+  void dump() {
+    ostringstream oss;
+    oss << "num:" << num << endl;
+    oss << "itsize:" << list_it_.size() << endl;
+    oss << "idssize:" << ids_.size() << endl;
+    for (auto id : ids_) {
+      oss << id << " ";
+    }
+    oss << endl;
+    cout << oss.str() << endl;
+  }
+};
 
 struct Llru {
   static int update(lua_State *L);
-  static int dump(lua_State *L);
 
   static int lru_gc(lua_State *L);
   static void lru_meta(lua_State *L);
   static int create_lru(lua_State *L);
 };
 
-int Llru::dump(lua_State *L) {
-  auto pp = (Lru **)luaL_checkudata(L, 1, LLRU_META);
-  auto &lru = **pp;
-
-  string s;
-  lru.dump(s);
-  lua_pushlstring(L, s.c_str(), s.size());
-  return 1;
-}
-
 int Llru::update(lua_State *L) {
   auto pp = (Lru **)luaL_checkudata(L, 1, LLRU_META);
   auto &lru = **pp;
-
   size_t len;
   const char *p = luaL_checklstring(L, 2, &len);
+
   string evict;
-
-  bool b = lru.update({p, len}, evict);
-  if (b) {
-    lua_pushlstring(L, evict.c_str(), evict.size());
-    return 1;
-  }
-
-  return 0;
+  lru.update({p, len}, evict);
+  if (evict.size() == 0) return 0;
+  lua_pushlstring(L, evict.c_str(), evict.size());
+  return 1;
 }
 
 int Llru::lru_gc(lua_State *L) {
@@ -52,7 +77,6 @@ void Llru::lru_meta(lua_State *L) {
   if (luaL_newmetatable(L, LLRU_META)) {
     luaL_Reg l[] = {
         {"update", update},
-        {"dump", dump},
         {NULL, NULL},
     };
     luaL_newlib(L, l);
@@ -64,10 +88,10 @@ void Llru::lru_meta(lua_State *L) {
 }
 
 int Llru::create_lru(lua_State *L) {
-  int cache_size = luaL_checkinteger(L, 1);
-
+  int num = luaL_checkinteger(L, 1);
+  if (num <= 0) return 0;
   Lru *p = new Lru();
-  p->cache_size_ = cache_size;
+  p->num = num;
   Lru **pp = (Lru **)lua_newuserdata(L, sizeof(p));
   *pp = p;
   lru_meta(L);
