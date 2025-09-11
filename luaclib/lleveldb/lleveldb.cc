80242e4f589573dd68082676c12a94d5ee883d6d
Monsoon <97245589@qq.com>
2025-09-11 18:47:55 +0000

    leveldb

--- luaclib/lleveldb/lleveldb.cc
+++ luaclib/lleveldb/lleveldb.cc
@@ -1,4 +1,5 @@
-extern "C" {
+extern "C"
+{
 #include "lauxlib.h"
 #include "lua.h"
 }
@@ -14,7 +15,8 @@ using namespace std;
 
 static const char *LLEVELDB_META = "LLEVELDB_META";
 
-struct Lleveldb {
+struct Lleveldb
+{
   static int create(lua_State *L);
   static void meta(lua_State *L);
   static int gc(lua_State *L);
@@ -36,13 +38,15 @@ struct Lleveldb {
   const static char split_ = 0xff;
 };
 
-int Lleveldb::hmget(lua_State *L) {
+int Lleveldb::hmget(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
   luaL_checktype(L, 2, LUA_TTABLE);
   int len = lua_rawlen(L, 2);
-  if (len < 2) {
+  if (len < 2)
+  {
     return luaL_error(L, "leveldb hmget len arr");
   }
   lua_createtable(L, len - 1, 0);
@@ -50,7 +54,8 @@ int Lleveldb::hmget(lua_State *L) {
   lua_rawgeti(L, 2, 1);
   const char *pk = luaL_checklstring(L, -1, &lk);
   string key{pk, lk};
-  for (int i = 2; i <= len; ++i) {
+  for (int i = 2; i <= len; ++i)
+  {
     lua_rawgeti(L, 2, i);
     size_t lhk;
     const char *phk = luaL_checklstring(L, -1, &lhk);
@@ -58,9 +63,12 @@ int Lleveldb::hmget(lua_State *L) {
     string rkey = key + split_ + hkey;
     string val;
     leveldb::Status s = db->Get(leveldb::ReadOptions(), rkey, &val);
-    if (s.ok()) {
+    if (s.ok())
+    {
       lua_pushlstring(L, val.c_str(), val.size());
-    } else {
+    }
+    else
+    {
       lua_pushnil(L);
     }
     lua_rawseti(L, 3, i - 1);
@@ -69,7 +77,8 @@ int Lleveldb::hmget(lua_State *L) {
   return 1;
 }
 
-int Lleveldb::hget(lua_State *L) {
+int Lleveldb::hget(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
@@ -83,15 +92,19 @@ int Lleveldb::hget(lua_State *L) {
 
   string val;
   leveldb::Status status = db->Get(leveldb::ReadOptions(), key, &val);
-  if (status.ok()) {
+  if (status.ok())
+  {
     lua_pushlstring(L, val.c_str(), val.size());
     return 1;
-  } else {
+  }
+  else
+  {
     return 0;
   }
 }
 
-int Lleveldb::hset(lua_State *L) {
+int Lleveldb::hset(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
@@ -109,7 +122,8 @@ int Lleveldb::hset(lua_State *L) {
   return 0;
 }
 
-int Lleveldb::hdel(lua_State *L) {
+int Lleveldb::hdel(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
@@ -125,14 +139,16 @@ int Lleveldb::hdel(lua_State *L) {
   return 0;
 }
 
-int Lleveldb::realkeys(lua_State *L) {
+int Lleveldb::realkeys(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
   lua_createtable(L, 0, 0);
   int i = 1;
   leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
-  for (it->SeekToFirst(); it->Valid(); it->Next()) {
+  for (it->SeekToFirst(); it->Valid(); it->Next())
+  {
     const string &k = it->key().ToString();
     lua_pushlstring(L, k.c_str(), k.size());
     lua_rawseti(L, -2, i++);
@@ -142,19 +158,23 @@ int Lleveldb::realkeys(lua_State *L) {
 }
 
 void Lleveldb::search_key(leveldb::DB *&db, string str,
-                          function<void(string, string, string)> func) {
+                          function<void(string, string, string)> func)
+{
   string start = str + split_;
   string end = start + char(0xff);
 
   leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
 
-  for (it->Seek(start); it->Valid() && it->key().ToString() < end; it->Next()) {
+  for (it->Seek(start); it->Valid() && it->key().ToString() < end; it->Next())
+  {
     string k = it->key().ToString();
 
-    if (k.size() <= start.size()) {
+    if (k.size() <= start.size())
+    {
       continue;
     }
-    if (k.substr(0, start.size()) != start) {
+    if (k.substr(0, start.size()) != start)
+    {
       continue;
     }
 
@@ -166,24 +186,29 @@ void Lleveldb::search_key(leveldb::DB *&db, string str,
   delete it;
 }
 
-int Lleveldb::keys(lua_State *L) {
+int Lleveldb::keys(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
-  if (!lua_isstring(L, 2)) {
+  if (!lua_isstring(L, 2))
+  {
     return luaL_error(L, "keys param 2 err");
   }
 
   vector<string> keys;
   leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
-  for (it->SeekToFirst(); it->Valid(); it->Next()) {
+  for (it->SeekToFirst(); it->Valid(); it->Next())
+  {
     string k = it->key().ToString();
     int p = k.find(split_);
-    if (p < 0 || p >= k.size() - 1) {
+    if (p < 0 || p >= k.size() - 1)
+    {
       continue;
     }
     string key = k.substr(0, p);
-    if (keys.empty() || keys.back() != key) {
+    if (keys.empty() || keys.back() != key)
+    {
       keys.push_back(key);
     }
   }
@@ -191,14 +216,16 @@ int Lleveldb::keys(lua_State *L) {
 
   lua_createtable(L, keys.size(), 0);
   int i = 0;
-  for (auto key : keys) {
+  for (auto key : keys)
+  {
     lua_pushlstring(L, key.c_str(), key.size());
     lua_rawseti(L, -2, ++i);
   }
   return 1;
 }
 
-int Lleveldb::del(lua_State *L) {
+int Lleveldb::del(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
@@ -208,19 +235,20 @@ int Lleveldb::del(lua_State *L) {
 
   leveldb::WriteBatch batch;
 
-  search_key(db, str, [&](string key, string val, string realkey) {
-    batch.Delete(realkey);
-  });
+  search_key(db, str, [&](string key, string val, string realkey)
+             { batch.Delete(realkey); });
 
   db->Write(leveldb::WriteOptions(), &batch);
   return 0;
 }
 
-int Lleveldb::hgetall(lua_State *L) {
+int Lleveldb::hgetall(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
-  if (lua_isnil(L, 2)) {
+  if (lua_isnil(L, 2))
+  {
     return luaL_error(L, "hgetall no key");
   }
 
@@ -230,23 +258,25 @@ int Lleveldb::hgetall(lua_State *L) {
 
   lua_createtable(L, 0, 0);
   int i = 0;
-  search_key(db, str, [&](string key, string val, string realkey) {
+  search_key(db, str, [&](string key, string val, string realkey)
+             {
     lua_pushlstring(L, key.c_str(), key.size());
     lua_rawseti(L, -2, ++i);
     lua_pushlstring(L, val.c_str(), val.size());
-    lua_rawseti(L, -2, ++i);
-  });
+    lua_rawseti(L, -2, ++i); });
 
   return 1;
 }
 
-int Lleveldb::hmset(lua_State *L) {
+int Lleveldb::hmset(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   leveldb::DB *db = *pp;
 
   luaL_checktype(L, 2, LUA_TTABLE);
   uint32_t len = lua_rawlen(L, 2);
-  if (len < 3 || len % 2 == 0) {
+  if (len < 3 || len % 2 == 0)
+  {
     return luaL_error(L, "leveldb hmset len err");
   }
 
@@ -255,7 +285,8 @@ int Lleveldb::hmset(lua_State *L) {
   lua_rawgeti(L, 2, 1);
   const char *pk = luaL_checklstring(L, 3, &lk);
   string key{pk, lk};
-  for (int i = 2; i <= len - 1; i += 2) {
+  for (int i = 2; i <= len - 1; i += 2)
+  {
     lua_settop(L, 2);
     lua_rawgeti(L, 2, i);
     size_t lhk;
@@ -272,19 +303,18 @@ int Lleveldb::hmset(lua_State *L) {
   return 0;
 }
 
-int Lleveldb::gc(lua_State *L) {
+int Lleveldb::gc(lua_State *L)
+{
   leveldb::DB **pp = (leveldb::DB **)luaL_checkudata(L, 1, LLEVELDB_META);
   delete *pp;
   return 0;
 }
 
-void Lleveldb::meta(lua_State *L) {
-  if (luaL_newmetatable(L, LLEVELDB_META)) {
-    luaL_Reg l[] = {{"del", del},           {"hmset", hmset},
-                    {"hmget", hmget},       {"hgetall", hgetall},
-                    {"hget", hget},         {"hset", hset},
-                    {"hdel", hdel},         {"keys", keys},
-                    {"realkeys", realkeys}, {NULL, NULL}};
+void Lleveldb::meta(lua_State *L)
+{
+  if (luaL_newmetatable(L, LLEVELDB_META))
+  {
+    luaL_Reg l[] = {{"del", del}, {"hmset", hmset}, {"hmget", hmget}, {"hgetall", hgetall}, {"hget", hget}, {"hset", hset}, {"hdel", hdel}, {"keys", keys}, {"realkeys", realkeys}, {NULL, NULL}};
     luaL_newlib(L, l);
     lua_setfield(L, -2, "__index");
     lua_pushcfunction(L, gc);
@@ -293,7 +323,8 @@ void Lleveldb::meta(lua_State *L) {
   lua_setmetatable(L, -2);
 }
 
-int Lleveldb::create(lua_State *L) {
+int Lleveldb::create(lua_State *L)
+{
   size_t len;
   const char *p = luaL_checklstring(L, 1, &len);
 
@@ -301,9 +332,13 @@ int Lleveldb::create(lua_State *L) {
   leveldb::Options options;
   options.create_if_missing = true;
   options.compression = leveldb::kNoCompression;
+  options.write_buffer_size = 16 * 1024 * 1024;
+  options.max_file_size = 8 * 1024 * 1024;
+  options.block_size = 16 * 1024;
   leveldb::Status status = leveldb::DB::Open(options, {p, len}, &db);
 
-  if (!status.ok()) {
+  if (!status.ok())
+  {
     return luaL_error(L, "leveldb open err");
   }
 
@@ -313,10 +348,12 @@ int Lleveldb::create(lua_State *L) {
   return 1;
 }
 
-extern "C" {
-LUAMOD_API int luaopen_lleveldb(lua_State *L) {
-  luaL_Reg funcs[] = {{"create", Lleveldb::create}, {NULL, NULL}};
-  luaL_newlib(L, funcs);
-  return 1;
-}
+extern "C"
+{
+  LUAMOD_API int luaopen_lleveldb(lua_State *L)
+  {
+    luaL_Reg funcs[] = {{"create", Lleveldb::create}, {NULL, NULL}};
+    luaL_newlib(L, funcs);
+    return 1;
+  }
 }
