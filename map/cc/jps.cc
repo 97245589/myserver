5be65deea28700e842588bcde094cee6f7336e13
monsoon <97245589@qq.com>
2025-09-12 21:05:31 -0700

    format google

--- map/cc/jps.cc
+++ map/cc/jps.cc
@@ -10,16 +10,12 @@ using namespace std;
 static const int16_t sweigh = 100;
 static const int16_t dweigh = 141;
 
-struct Pos_direct_cmp
-{
-  bool operator()(const Pos &lhs, const Pos &rhs)
-  {
+struct Pos_direct_cmp {
+  bool operator()(const Pos &lhs, const Pos &rhs) {
     int dx = end_.x_ - start_.x_;
     int dy = end_.y_ - start_.y_;
-    if (dx)
-      dx = dx > 0 ? 1 : -1;
-    if (dy)
-      dy = dy > 0 ? 1 : -1;
+    if (dx) dx = dx > 0 ? 1 : -1;
+    if (dy) dy = dy > 0 ? 1 : -1;
     int l = abs(lhs.x_ - dx) + abs(lhs.y_ - dy);
     int r = abs(rhs.x_ - dx) + abs(rhs.y_ - dy);
     return l < r;
@@ -28,17 +24,16 @@ struct Pos_direct_cmp
   Pos end_;
 };
 
-static vector<Pos> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+static vector<Pos> dirs = {{0, 1}, {0, -1}, {1, 0},  {-1, 0},
+                           {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
 static map<Pos, int8_t> dir_idx = {
     {{0, 1}, 0}, {{0, -1}, 1}, {{1, 0}, 2}, {{-1, 0}, 3}};
 
-bool Jps::add_force_neighbor(State s, Pos dir)
-{
+bool Jps::add_force_neighbor(State s, Pos dir) {
   bool b = false;
   Pos ps = {s.x_, s.y_};
 
-  force_neighbor(ps, dir, [&](Pos p, Pos dir)
-                 {
+  force_neighbor(ps, dir, [&](Pos p, Pos dir) {
     auto &pres = *ppres_;
     if (auto it = pres.find(p); it != pres.end()) return;
     State news;
@@ -49,20 +44,18 @@ bool Jps::add_force_neighbor(State s, Pos dir)
     news.dx_ = dir.x_;
     news.dy_ = dir.y_;
     cal_weigh(news);
-    add_jp(news, ps); 
-    b = true; });
+    add_jp(news, ps);
+    b = true;
+  });
   return b;
 }
 
-bool Jps::add_jp_to_openlist(State p, Pos dir)
-{
-  if (dir.x_ && dir.y_)
-    return false;
+bool Jps::add_jp_to_openlist(State p, Pos dir) {
+  if (dir.x_ && dir.y_) return false;
   State s = p;
   Pos ps = {s.x_, s.y_};
   int len = search_jp_cache(ps, dir);
-  if (len <= 0)
-    return false;
+  if (len <= 0) return false;
 
   s.x_ += dir.x_ * len;
   s.y_ += dir.y_ * len;
@@ -70,10 +63,8 @@ bool Jps::add_jp_to_openlist(State p, Pos dir)
 
   bool b = add_force_neighbor(s, dir);
   auto &pres = *ppres_;
-  if (b)
-  {
-    if (pres.find({s.x_, s.y_}) == pres.end())
-    {
+  if (b) {
+    if (pres.find({s.x_, s.y_}) == pres.end()) {
       s.dx_ = dir.x_;
       s.dy_ = dir.y_;
       cal_weigh(s);
@@ -83,15 +74,12 @@ bool Jps::add_jp_to_openlist(State p, Pos dir)
   return b;
 }
 
-void Jps::add_jp(State s, Pos pre)
-{
+void Jps::add_jp(State s, Pos pre) {
   Pos ps = {s.x_, s.y_};
-  if (ps == pre)
-    return;
+  if (ps == pre) return;
   auto &open_list = *popen_list_;
   auto &pres = *ppres_;
-  if (pres.find(ps) != pres.end())
-    return;
+  if (pres.find(ps) != pres.end()) return;
   // cout << "addjp " << s.x_ << "," << s.y_ << " " << (int)(s.dx_) << ","
   //      << (int)(s.dy_) << " " << s.cost_ << " " << s.weigh_ << " " << pre.x_
   //      << "," << pre.y_ << endl;
@@ -99,25 +87,18 @@ void Jps::add_jp(State s, Pos pre)
   pres[ps] = pre;
 }
 
-bool Jps::find_end(Pos p, Pos dir)
-{
-  if (dir.x_ && dir.y_)
-    return false;
-  if (p == end_)
-    return true;
+bool Jps::find_end(Pos p, Pos dir) {
+  if (dir.x_ && dir.y_) return false;
+  if (p == end_) return true;
 
   int dx = end_.x_ - p.x_;
   int dy = end_.y_ - p.y_;
-  if (dx)
-    dx = dx > 0 ? 1 : -1;
-  if (dy)
-    dy = dy > 0 ? 1 : -1;
-  if (dx != dir.x_ || dy != dir.y_)
-    return false;
+  if (dx) dx = dx > 0 ? 1 : -1;
+  if (dy) dy = dy > 0 ? 1 : -1;
+  if (dx != dir.x_ || dy != dir.y_) return false;
 
   int16_t len = search_jp_cache(p, dir);
-  if (len <= 0)
-    return false;
+  if (len <= 0) return false;
   Pos newp;
   newp.x_ = p.x_ + dx * len;
   newp.y_ = p.y_ + dy * len;
@@ -130,57 +111,43 @@ bool Jps::find_end(Pos p, Pos dir)
   return false;
 }
 
-void Jps::cal_weigh(State &s)
-{
+void Jps::cal_weigh(State &s) {
   Pos p{s.x_, s.y_};
-  if (p == end_)
-  {
+  if (p == end_) {
     s.weigh_ = 0;
     return;
   }
   int distance = dis(p, end_);
-  if (!quick_)
-  {
+  if (!quick_) {
     s.weigh_ = distance + s.cost_;
-  }
-  else
-  {
+  } else {
     s.weigh_ = distance;
   }
 }
 
-void Jps::force_neighbor(Pos p, Pos dir, function<void(Pos, Pos)> cb)
-{
-  if (dir.x_ && dir.y_)
-    return;
+void Jps::force_neighbor(Pos p, Pos dir, function<void(Pos, Pos)> cb) {
+  if (dir.x_ && dir.y_) return;
   Pos p1 = {(int16_t)(p.x_ + dir.y_), (int16_t)(p.y_ + dir.x_)};
   Pos p11 = {(int16_t)(p1.x_ + dir.x_), (int16_t)(p1.y_ + dir.y_)};
-  if (side_check(p1) && side_check(p11))
-  {
-    if (!walkable(p1) && walkable(p11))
-    {
+  if (side_check(p1) && side_check(p11)) {
+    if (!walkable(p1) && walkable(p11)) {
       cb(p11, {(int16_t)(dir.x_ + dir.y_), (int16_t)(dir.y_ + dir.x_)});
     }
   }
 
   Pos p2 = {(int16_t)(p.x_ - dir.y_), (int16_t)(p.y_ - dir.x_)};
   Pos p21 = {(int16_t)(p2.x_ + dir.x_), (int16_t)(p2.y_ + dir.y_)};
-  if (side_check(p2) && side_check(p21))
-  {
-    if (!walkable(p2) && walkable(p21))
-    {
+  if (side_check(p2) && side_check(p21)) {
+    if (!walkable(p2) && walkable(p21)) {
       cb(p21, {(int16_t)(dir.x_ - dir.y_), (int16_t)(dir.y_ - dir.x_)});
     }
   }
 }
 
-string Jps::dump_jp_cache(Pos dir)
-{
+string Jps::dump_jp_cache(Pos dir) {
   ostringstream oss;
-  for (int16_t j = world_.wid_ - 1; j >= 0; --j)
-  {
-    for (int16_t i = 0; i < world_.len_; ++i)
-    {
+  for (int16_t j = world_.wid_ - 1; j >= 0; --j) {
+    for (int16_t i = 0; i < world_.len_; ++i) {
       int val = search_jp_cache({i, j}, dir);
       oss << setw(3) << val;
     }
@@ -189,44 +156,33 @@ string Jps::dump_jp_cache(Pos dir)
   return oss.str();
 }
 
-int Jps::search_jp_cache(Pos p, Pos dir)
-{
+int Jps::search_jp_cache(Pos p, Pos dir) {
   auto &arr = jp_cache_[p.x_][p.y_];
   return arr[dir_idx[dir]];
 }
 
-void Jps::add_jp_cache(Pos p, Pos dir, int16_t len)
-{
+void Jps::add_jp_cache(Pos p, Pos dir, int16_t len) {
   jp_cache_[p.x_][p.y_][dir_idx[dir]] = len;
 }
 
-void Jps::line_jp_cache(Pos p, Pos dir)
-{
-  if (dir.x_ && dir.y_)
-    return;
+void Jps::line_jp_cache(Pos p, Pos dir) {
+  if (dir.x_ && dir.y_) return;
   Pos q = p;
-  while (side_check(q) && !walkable(q))
-  {
+  while (side_check(q) && !walkable(q)) {
     add_jp_cache(q, dir, 0);
     q.x_ += dir.x_;
     q.y_ += dir.y_;
   }
-  if (!side_check(q))
-    return;
+  if (!side_check(q)) return;
 
   Pos t = q;
   int i = 0;
-  while (1)
-  {
+  while (1) {
     bool fneig = false;
-    force_neighbor(t, dir, [&](Pos p1, Pos p2)
-                   { fneig = true; });
-    if (!walkable(t) || (fneig && i))
-    {
-      if (!walkable(t))
-        --i;
-      for (int16_t j = 0; j <= i; ++j)
-      {
+    force_neighbor(t, dir, [&](Pos p1, Pos p2) { fneig = true; });
+    if (!walkable(t) || (fneig && i)) {
+      if (!walkable(t)) --i;
+      for (int16_t j = 0; j <= i; ++j) {
         Pos t2{(int16_t)(q.x_ + dir.x_ * j), (int16_t)(q.y_ + dir.y_ * j)};
         add_jp_cache(t2, dir, i - j);
       }
@@ -241,78 +197,59 @@ void Jps::line_jp_cache(Pos p, Pos dir)
 }
 
 void Jps::block_jp_cache(int16_t minx, int16_t miny, int16_t maxx,
-                         int16_t maxy)
-{
-  if (minx > maxx || miny > maxy)
-    return;
-  if (!side_check({minx, miny}) || !side_check({maxx, maxy}))
-    return;
-  for (int16_t i = minx - 1; i <= maxx + 1; ++i)
-  {
-    if (i < 0 || i > world_.len_ - 1)
-      continue;
+                         int16_t maxy) {
+  if (minx > maxx || miny > maxy) return;
+  if (!side_check({minx, miny}) || !side_check({maxx, maxy})) return;
+  for (int16_t i = minx - 1; i <= maxx + 1; ++i) {
+    if (i < 0 || i > world_.len_ - 1) continue;
     line_jp_cache({i, 0}, {0, 1});
     line_jp_cache({i, (int16_t)(world_.wid_ - 1)}, {0, -1});
   }
-  for (int16_t j = miny - 1; j <= maxy + 1; ++j)
-  {
-    if (j < 0 || j > world_.wid_ - 1)
-      continue;
+  for (int16_t j = miny - 1; j <= maxy + 1; ++j) {
+    if (j < 0 || j > world_.wid_ - 1) continue;
     line_jp_cache({0, j}, {1, 0});
     line_jp_cache({(int16_t)(world_.len_ - 1), j}, {-1, 0});
   }
 }
 
-void Jps::init_jp_cache()
-{
+void Jps::init_jp_cache() {
   jp_cache_ = vector<vector<array<int16_t, 4>>>(
       world_.wid_, vector<array<int16_t, 4>>(world_.len_));
-  for (int16_t i = 0; i < world_.len_; ++i)
-  {
+  for (int16_t i = 0; i < world_.len_; ++i) {
     line_jp_cache({i, 0}, {0, 1});
     line_jp_cache({i, (int16_t)(world_.wid_ - 1)}, {0, -1});
   }
 
-  for (int16_t j = 0; j < world_.wid_; ++j)
-  {
+  for (int16_t j = 0; j < world_.wid_; ++j) {
     line_jp_cache({0, j}, {1, 0});
     line_jp_cache({(int16_t)(world_.len_ - 1), j}, {-1, 0});
   }
 }
 
-int Jps::dis(Pos p1, Pos p2)
-{
+int Jps::dis(Pos p1, Pos p2) {
   double d = pow((p2.x_ - p1.x_), 2) + pow((p2.y_ - p1.y_), 2);
   return sqrt(d) * 100;
 }
 
-bool Jps::step(State start, Pos dir)
-{
+bool Jps::step(State start, Pos dir) {
   // cout << "step" << start.x_ << start.y_ << " " << (int)(dir.x_)
   //      << (int)(dir.y_) << " " << endl;
   start.dx_ = dir.x_;
   start.dy_ = dir.y_;
   State s = start;
 
-  while (1)
-  {
-    if (!dir.x_ || !dir.y_)
-    {
+  while (1) {
+    if (!dir.x_ || !dir.y_) {
       Pos ps = {s.x_, s.y_};
-      if (!walkable(ps))
-        return false;
-      if (find_end(ps, dir))
-      {
+      if (!walkable(ps)) return false;
+      if (find_end(ps, dir)) {
         over_ = true;
         add_jp({end_.x_, end_.y_, 0, 0, 0, 0}, ps);
         return true;
       }
-      if (add_jp_to_openlist(s, dir))
-        return true;
+      if (add_jp_to_openlist(s, dir)) return true;
       return false;
-    }
-    else if (dir.x_ && dir.y_)
-    {
+    } else if (dir.x_ && dir.y_) {
       Pos pstart = {start.x_, start.y_};
       Pos ps = {s.x_, s.y_};
       // cout << "===" << s.x_ << s.y_ << " " << (int)(dir.x_) << (int)(dir.y_)
@@ -321,33 +258,31 @@ bool Jps::step(State start, Pos dir)
       auto &pres = *ppres_;
       b = add_force_neighbor(s, {dir.x_, 0}) || b;
       b = add_force_neighbor(s, {0, dir.y_}) || b;
-      if (b)
-      {
-        if (!(ps == pstart) && pres.end() == pres.find(ps))
-        {
+      if (b) {
+        if (!(ps == pstart) && pres.end() == pres.find(ps)) {
           pres[ps] = pstart;
         }
         int x = s.x_;
         int y = s.y_;
         int dx = dir.x_;
         int dy = dir.y_;
-        bool b1 = side_check({(int16_t)(x + dx), (int16_t)y}) && !walkable({(int16_t)(x + dx), (int16_t)y});
-        bool b2 = side_check({(int16_t)x, (int16_t)(y + dy)}) && !walkable({(int16_t)x, (int16_t)(y + dy)});
-        bool b3 = side_check({(int16_t)(x + dx), (int16_t)(y + dy)}) && walkable({(int16_t)(x + dx), (int16_t)(y + dy)});
+        bool b1 = side_check({(int16_t)(x + dx), (int16_t)y}) &&
+                  !walkable({(int16_t)(x + dx), (int16_t)y});
+        bool b2 = side_check({(int16_t)x, (int16_t)(y + dy)}) &&
+                  !walkable({(int16_t)x, (int16_t)(y + dy)});
+        bool b3 = side_check({(int16_t)(x + dx), (int16_t)(y + dy)}) &&
+                  walkable({(int16_t)(x + dx), (int16_t)(y + dy)});
         // cout << x << y << " " << dx << dy << endl;
         // cout << "check:" << b1 << b2 << b3 << endl;
-        if ((b1 || b2) && b3)
-          return true;
+        if ((b1 || b2) && b3) return true;
       }
 
       s.x_ += dir.x_;
       s.y_ += dir.y_;
       s.cost_ += dweigh;
       ps = {s.x_, s.y_};
-      if (!walkable(ps))
-        return false;
-      if (ps == end_)
-      {
+      if (!walkable(ps)) return false;
+      if (ps == end_) {
         over_ = true;
         s.weigh_ = 0;
         add_jp(s, pstart);
@@ -358,18 +293,15 @@ bool Jps::step(State start, Pos dir)
       bool r = false;
       r = step(s, {dir.x_, 0}) || r;
       r = step(s, {0, dir.y_}) || r;
-      if (r)
-      {
-        if (pres.end() == pres.find(ps))
-        {
+      if (r) {
+        if (pres.end() == pres.find(ps)) {
           pres[ps] = pstart;
           s.x_ += dir.x_;
           s.y_ += dir.y_;
           s.cost_ += dweigh;
           cal_weigh(s);
           ps = {s.x_, s.y_};
-          if (walkable(ps) && pres.end() == pres.find(ps))
-          {
+          if (walkable(ps) && pres.end() == pres.find(ps)) {
             add_jp(s, pstart);
           }
           return true;
@@ -379,17 +311,13 @@ bool Jps::step(State start, Pos dir)
   }
 }
 
-void Jps::pathfind(Pos s, Pos e, vector<Pos> &ret)
-{
+void Jps::pathfind(Pos s, Pos e, vector<Pos> &ret) {
   over_ = false;
   start_ = s;
   end_ = e;
-  if (!walkable(start_))
-    return;
-  if (!walkable(end_))
-    return;
-  if (s == e)
-    return;
+  if (!walkable(start_)) return;
+  if (!walkable(end_)) return;
+  if (s == e) return;
 
   set<Pos> close_list;
   map<Pos, Pos> pres;
@@ -400,21 +328,17 @@ void Jps::pathfind(Pos s, Pos e, vector<Pos> &ret)
   int weigh = dis(s, e);
   open_list.push({s.x_, s.y_, 0, weigh, 0, 0});
 
-  while (!open_list.empty())
-  {
+  while (!open_list.empty()) {
     State st = open_list.top();
     open_list.pop();
     Pos pt = {.x_ = st.x_, .y_ = st.y_};
-    if (close_list.find(pt) != close_list.end())
-      continue;
+    if (close_list.find(pt) != close_list.end()) continue;
     close_list.insert(pt);
 
-    if (end_ == pt)
-    {
+    if (end_ == pt) {
       ret.push_back(end_);
       Pos p = end_;
-      while (pres.end() != pres.find(p))
-      {
+      while (pres.end() != pres.find(p)) {
         p = pres[p];
         ret.push_back(p);
       }
@@ -424,27 +348,20 @@ void Jps::pathfind(Pos s, Pos e, vector<Pos> &ret)
     int8_t dx = st.dx_;
     int8_t dy = st.dy_;
     vector<Pos> dirs_;
-    if (!dx && !dy)
-    {
+    if (!dx && !dy) {
       Pos_direct_cmp p = {.start_ = s, .end_ = e};
       dirs_ = dirs;
       sort(dirs_.begin(), dirs_.end(), p);
-    }
-    else if (dx && dy)
-    {
+    } else if (dx && dy) {
       dirs_.push_back({dx, 0});
       dirs_.push_back({0, dy});
       dirs_.push_back({dx, dy});
-    }
-    else
-    {
+    } else {
       dirs_.push_back({dx, dy});
     }
-    for (auto dir : dirs_)
-    {
+    for (auto dir : dirs_) {
       step(st, dir);
-      if (over_)
-        break;
+      if (over_) break;
     }
   }
   popen_list_ = nullptr;
